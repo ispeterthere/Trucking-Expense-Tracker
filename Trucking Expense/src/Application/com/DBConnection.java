007821b203b5ae60b6aaf752531aca6f20cfd0d2
ispeterthere <45906868+ispeterthere@users.noreply.github.com>
2019-04-05 12:46:28 -0400

    New UI
    Added new Login UI
    Added new Sign Up UI

--- Trucking Expense/src/Application/com/DBConnection.java
+++ Trucking Expense/src/Application/com/DBConnection.java
@@ -7,30 +7,23 @@ public class DBConnection {
     private static final String url = "jdbc:mysql://localhost:3306/mydb";
     private static final String username = "root";
     private static final String password = "12345";
-    private static String driverName = "com.mysql.jdbc.Driver";
     private static Connection connection;
 
     public static Connection getConnection() throws SQLException
     {
-        try {
-            Class.forName(driverName);
-            try
-            {
-                connection = DriverManager.getConnection(url, username, password);
-            } catch(SQLException ex)
-            {
-                throw new RuntimeException("Error connecting to the database...", ex);
-            }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        try
+        {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch(SQLException ex)
+        {
+            throw new RuntimeException("Error connecting to the database...", ex);
         }
         return connection;
 
     }
 
 
-
-
-
-
+    public Statement createStatement() throws SQLException {
+      return connection.createStatement();
+    }
 }
